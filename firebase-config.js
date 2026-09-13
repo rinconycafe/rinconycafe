d49cd9216e58d793cc8e9f9982ee1cb1200c9981
rinconycafe <rinconycafe@gmail.com>
2026-09-12 21:52:51 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// ============================================
+// CONFIGURACIÓN DE FIREBASE
+// RINCÓN & CAFÉ
+// ============================================
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBfbHhLMHDyuWqy7zlKGXsNM6d52ePUAYo",
+  authDomain: "rincon-y-cafe.firebaseapp.com",
+  databaseURL: "https://rincon-y-cafe-default-rtdb.firebaseio.com",
+  projectId: "rincon-y-cafe",
+  storageBucket: "rincon-y-cafe.firebasestorage.app",
+  messagingSenderId: "536292321999",
+  appId: "1:536292321999:web:a10dc688a8c2249810a5ba"
+};
+
+export default firebaseConfig;
